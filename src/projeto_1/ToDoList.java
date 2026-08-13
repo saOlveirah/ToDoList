56249d9db59f6e51a9f01a41237f0aa22f5d8a37
Heitor Oliveira <heitor.o.sa@aluno.senai.br>
2026-08-13 10:57:16 -0300

    alterações aula 13/08

--- src/projeto_1/ToDoList.java
+++ src/projeto_1/ToDoList.java
@@ -4,10 +4,29 @@
  */
 package projeto_1;
 
-/**
- *
- * @author Aluno
- */
+import java.util.Scanner;
+
+
 public class ToDoList {
     
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);  
+    
+        String[] tarefas = new String[10];
+        boolean[] concluidas = new boolean[10];
+    
+        int opcao = 0;
+    
+        while (opcao !=5) {
+        System.out.println("Lista de tarefas");
+        System.out.println("1- Adicionar tarefa");
+        System.out.println("2- Listar Tarefa");
+        System.out.println("3- Concluir tarefa");
+        System.out.println("4- Excluir tarefa");
+        System.out.println("5- Sair");
+    
+        System.out.println("Digite a opcao que voce quer: ");
+        int escolha = scanner.nextInt();
+        }
+    }
 }
