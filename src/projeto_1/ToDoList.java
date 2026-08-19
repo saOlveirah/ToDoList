2ff6d37bfaaa1e54470645263c3c984a2f9c64dd
Heitor <heitor.o.sa@aluno.senai.br>
2026-08-19 11:01:30 -0300

    alterações do dia 19/08

--- src/projeto_1/ToDoList.java
+++ src/projeto_1/ToDoList.java
@@ -27,6 +27,65 @@ public class ToDoList {
     
         System.out.println("Digite a opcao que voce quer: ");
         int escolha = scanner.nextInt();
+        
+ switch(opcao) {
+            case 1: 
+                int posicaoLivre = -1;
+                
+                for(int i = 0; i < tarefas.length; i++){
+                    if (tarefas [i] == null){
+                        posicaoLivre = 1;
+                        break;
+                    } 
+                }
+            if(posicaoLivre == -1){
+                System.out.println("A lista de tarefas esta cheia!");
+            }else{
+                System.out.println("Digite a tarefa:");
+                tarefas[posicaoLivre] = scanner.nextLine();
+                concluidas[posicaoLivre] = false;
+                System.out.println("Tarefa adicionada com sucesso");
+            }
+            break;  
+            
+        case 2:
+            System.out.println("====MINHAS TAREFAS=====");
+            
+            boolean existeTarefa = false;
+            
+            for(int i = 0;  i < tarefas.length; i++){
+                if(tarefas[i] != null){
+                    existeTarefa = true;
+                    
+                    if(concluidas[i]){
+                        System.out.println((i + 1) + "- [x] " + tarefas[i]);
+                    }else{
+                        System.out.println((i + 1) + "- [] " + tarefas[i]);
+                    }
+                }
+            } 
+        
+            if (existeTarefa == false){
+                System.out.println("Nenhuma tarefa cadastrada:");
+            }
+            
+            break;
+           
+        case 3:
+            System.out.println("Digite o numero da tarefa que deseja concluir");
+            int numeroConcluir = scanner.nextInt();
+            
+            int indiceConcluir = numeroConcluir -1;
+            
+            if(indiceConcluir >= 0 && indiceConcluir < tarefas.length && tarefas[indiceConcluir] != null){
+               
+                concluidas[indiceConcluir] = true;
+               
+                System.out.println("Tarefa concluida com sucesso");
+                }else{
+                System.out.println("Tarefa invalida!");
+            }
+        } 
         }
     }
 }
